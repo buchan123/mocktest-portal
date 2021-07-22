06fcc0c42acf7251ecba24f47125c3ebf926ac3a
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2021-07-23 00:01:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,27 +21,26 @@ def home():
 def tests():
     return render_template("tests.html", email=current_user.name)
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SESSION_TYPE'] = 'memcached'
-    app.config['SECRET_KEY'] = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-    @login_manager.user_loader
-    def load_user(user_id):
-        try:
-            response = requests.get("https://mocktest-api.herokuapp.com/api/user/"+user_id)
-            email = response.json()["email"]
-            name = response.json()['name']
-            return User(email,name,user_id)
-        except:
-            return None
 
-    app.register_blueprint(auth_blueprint)
-    app.register_blueprint(main_blueprint)
-    return app
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 't1NP63m4wnBg6nyHYKfmc2TpCOGI4nss'
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        response = requests.get("https://mocktest-api.herokuapp.com/api/user/"+user_id)
+        email = response.json()["email"]
+        name = response.json()['name']
+        return User(email,name,user_id)
+    except:
+        return None
+
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(main_blueprint)
+    
 
 if __name__ == "__main__":
-    app = create_app()
     app.run() 
